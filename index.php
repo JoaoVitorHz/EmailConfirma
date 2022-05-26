f31145ffcd5ca1d7219143931bd80743f36668fb
João Vitor <vitorjoao39207@gmail.com>
2022-05-25 21:00:11 -0300

    Primeiro commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+if(isset($_POST['nome1'])  && !empty($_POST['name'])){
+   $nome addslashes($_POST['none']);
+   $email addslashes($_POST['email']);
+
+   require 'config.php';
+
+   $pdo-query("INSERT INTO usuarios SET none'$nome', email = '$email'");
+   $id = $pdo->lastInsertId();
+
+   $md5->md5($id);
+   $link= 'localhost/cadastrocomaprovação/confirmar.php?h='.$md5; //link para arquivo de confirmação
+   $assunto = "Confirme seu cadastro";
+   $msg = "Clique no link abaixo para confirmar seu cadastro:\n\n".$link;
+   $headers = "From:vitorjoao39207@gmail.com"."\r\n". 
+              "X-Mailer: PHP/".phpversion();
+
+   mail($email, $assunto, $msg, $headers);
+   echo"<h2>OK!Confirme seu cadastro agora!</h2>";
+}
+?>
+<form method="POST">
+   Nome:<br/>
+   <input type="text"nane="nome"/><br/><br/>
+   E-mail:<br/>
+   <input type="email" name="email"/><br/><br/>
+
+   <input type="submit" value="Cadastrar"/>
+</form>
---
+++ usuarios.sql
@@ -0,0 +1,60 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 26-Maio-2022 às 01:52
+-- Versão do servidor: 10.4.24-MariaDB
+-- versão do PHP: 8.1.5
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `test`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(50) CHARACTER SET utf8 DEFAULT NULL,
+  `email` varchar(100) CHARACTER SET utf8 DEFAULT NULL,
+  `status` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
